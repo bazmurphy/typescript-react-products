1a1dedb2fe918c6951ddd1adca65d9182344a54e
Baz <61154071+bazmurphy@users.noreply.github.com>
2023-08-01 17:27:54 +0100

    added useProducts and useCart custom hooks

---
+++ src/hooks/useCart.tsx
@@ -0,0 +1,11 @@
+// create a simple custom hook
+import { useContext } from "react";
+
+import CartContext from "../context/CartProvider";
+import { UseCartContextType } from "../context/CartProvider";
+
+const useCart = (): UseCartContextType => {
+  return useContext(CartContext);
+};
+
+export default useCart;
